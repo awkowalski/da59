bb90aa386c76a3c5263d1d668e065030c1fa6d54
A.K <adrian.kowalski87@gmail.com>
2017-09-11 21:27:32 +0200

    fixed

--- ConsoleApplication5/Program.cs
+++ ConsoleApplication5/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleApplication5.library;
 
 namespace ConsoleApplication5
 {
     class Program
     {
+        private static string line;
+
         static void Main(string[] args)
         {
+            string inputPath = @"E:\ETG\HE\test.rtf";
+            string outputPath = @"E:\ETG\HE\test_output.rtf";
+        
+            RTF_Handler my_rtf = new RTF_Handler(inputPath, outputPath);
+
+            while((line = my_rtf.getLine()) != null)
+            {
+                my_rtf.replaceValues("!GENREM", line, "TEST");
+            }
+            my_rtf.close();
         }
     }
 }
---
+++ ConsoleApplication5/library/RTF_Handler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5.library
+{
+    class RTF_Handler
+    {
+        private StreamReader inputFile;
+        private StreamWriter outputFile;
+        private Regex placeHolder;
+
+        public RTF_Handler(string input, string output)
+        {
+            inputFile = new StreamReader(input);
+            outputFile = new StreamWriter(output);
+        }
+
+        public void replaceValues(string token, string line, string outputValue)
+        {
+            placeHolder = new Regex(token);
+
+            if (token == "!GENREM" && placeHolder.IsMatch(line))
+            {
+                line = @"{\rtlch\fcs1 \af1\afs20 \ltrch\fcs0 \f1\fs20\cf2 }";
+                outputFile.WriteLine(line);
+                return;
+            }
+            placeHolder.Replace(line, outputValue);
+            outputFile.WriteLine(line);
+
+        }
+
+        public string  getLine()
+        {
+            try
+            {
+                return inputFile.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public bool close()
+        {
+            try
+            {
+                outputFile.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
